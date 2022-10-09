341ff1be0b222a7a41c2242b5eb76ad5b8779856
Judy Mwai <juddiemukami@gmail.com>
2022-10-09 14:04:57 +0300

    adds the login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,6 +3,29 @@ import React, {useState} from "react";
 function Login(){
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
+    return (
+        <div>
+            <form>
+                <input
+
+                    type="text"
+                    value={username}
+                    id= "username"
+                    onChange= {(e) => setUsername(e.target.value)}
+                    placeholder="Username"
+                />
+                <input
+                    type="text"
+                    value={password}
+                    id= "password"
+                    onChange= {(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+
+                />
+            </form>
+        </div>
+    )
     
 
 }
